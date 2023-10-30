a74d5eaefd8a5be3ca4a2e35069ff704c8a2950e
Michel <106674127+MichelZero@users.noreply.github.com>
2023-10-30 15:19:23 -0300

    up

--- ex04.py
+++ ex04.py
@@ -8,3 +8,20 @@
 # voltas nas 3 competições, e depois apresente qual a volta mais
 # rápida em cada competição.
 
+f1 = dict()
+for i in range(3):
+  pista = input(f"Digite o nome da pista[{i+1}]: ")
+  voltas = list()
+  while True:
+    tempo = float(input("Digite o tempo (0->Sair): "))
+    if tempo == 0:
+      break
+    voltas.append(tempo)
+  f1[pista] = voltas
+    
+# apresentando a volta mais rápida em cada pista e a média dos tempos
+for pista in f1.keys():
+  print(f"{pista} -> {f1[pista]}")
+  print(f"Volta mais rápida: {min(f1[pista])}")
+  print(f"Média dos tempos: {sum(f1[pista])/len(f1[pista])}")
+  print()
